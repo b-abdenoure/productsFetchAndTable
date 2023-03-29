a4a1db40547f6d75294602dc7e971315605e872a
b-abdenoure <nrboxworking@gmail.com>
2023-03-29 11:00:37 +0100

    Initial Commit

---
+++ productsFetchAndTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  productsFetchAndTable
+//
+//  Created by Abdenoure Boudlal on 3/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
