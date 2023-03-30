70f4148967e94b72df353da8f1fc86dcbef1b09b
b-abdenoure <nrboxworking@gmail.com>
2023-03-30 02:16:32 +0100

    first try following a tutorial and its work, i get the data unwrapped even i force unwrapping but i check the data json and there is a error handler in case its missing. next is to try to display the data into a table view using storyboard at first, then i will try a nib for more programmatic ui

--- productsFetchAndTable/ViewController.swift
+++ productsFetchAndTable/ViewController.swift
@@ -12,8 +12,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fetchProductsData { (products) in
+            
+            for product in products{
+                
+                print(product.title ?? "id")
+
+            }
+        }
     }
-
-
+    let productURL = "https://fakestoreapi.com/products"
+    
+    func fetchProductsData(completionHandler: @escaping ([Products]) -> Void){
+        let url = URL(string: productURL)!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            print(data)
+            guard let data = data else {return}
+            
+            do {
+                let postsData = try JSONDecoder().decode([Products].self, from: data)
+                
+                completionHandler(postsData)
+            }
+            catch{
+                let error = error
+                print(error.localizedDescription)
+            }
+        }
+        
+        .resume()
+    }
+    
 }
 
